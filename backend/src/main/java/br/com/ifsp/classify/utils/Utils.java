a027323f55590c32250cf5290b6b55f3ba994122
Gabriel <gabrielnakata@live.com>
2025-11-25 07:33:13 -0300

    feat: criado uma classe utils de propósitos gerais

---
+++ backend/src/main/java/br/com/ifsp/classify/utils/Utils.java
@@ -0,0 +1,8 @@
+package br.com.ifsp.classify.utils;
+
+public class Utils {
+
+    public static boolean isNullOrEmpty(String word) {
+        return word == null || word.trim().isEmpty();
+    }
+}
