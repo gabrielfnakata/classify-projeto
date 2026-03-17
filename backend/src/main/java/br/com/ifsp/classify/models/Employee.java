f9a5d000c58d6515223320e9e085b1c814da97c2
gfnakata <gabrielnakata@live.com>
2026-03-17 09:34:25 -0300

    feat: adiciona campo de senha

--- backend/src/main/java/br/com/ifsp/classify/models/Employee.java
+++ backend/src/main/java/br/com/ifsp/classify/models/Employee.java
@@ -8,12 +8,23 @@ import java.io.Serializable;
 @Table(name = "EMPLOYEE")
 public class Employee extends Person implements Serializable {
 
+    @Column(nullable = false, length = 30)
+    private String password;
+
     @Column(nullable = false, unique = true, length = 11)
     private String cpf;
 
     @ManyToOne
     @JoinColumn(name = "id", nullable = false)
     private Role cargo;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getCpf() {
         return cpf;
