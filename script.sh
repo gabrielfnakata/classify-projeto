834be48325d3c433d37ad942a46ff2e252a2e74d
gabrielfnakata <gabrielnakata@live.com>
2026-04-25 20:25:26 -0300

    chore: arrumado erro de portas já usadas

--- script.sh
+++ script.sh
@@ -19,32 +19,19 @@ askUser() {
 	echo ""
 	read -rsp "Digite sua senha de usuário root: " dbRootPassword
 	echo ""
-	read -rp "Em qual porta gostaria que o serviço rodasse (vazio para usar o padrão): " dbPort
 
 	echo ""
 	echo "- - - Backend - - -"
 	read -rp "Digite um JWT (Em branco para gerar aleatóriamente): " backJwtSecret
-	read -rp "Em qual porta gostaria que o serviço rodasse (vazio para usar o padrão): " backPort
-
-	echo ""
-	echo "- - - Frontend - - -"
-	read -rp "Em qual porta gostaria que o serviço rodasse (vazio para usar o padrão): " frontPort
 
 	echo "
 #MySQL
 MYSQL_USER=${dbUser}
 MYSQL_PASSWORD=${dbPassword}
 MYSQL_ROOT_PASSWORD=${dbRootPassword}
-MY_SQL_PORT=${dbPort:-3036}
 
 #Backend
 JWT_SECRET=${backJwtSecret:-$(generate_jwt)}
-BACKEND_PORT=${backPort:-8080}
-SPRING_DATASOURCE_URL=jdbc:mysql://db:3306/classify?createDatabaseIfNotExist=true&characterEncoding=UTF-8&serverTimezone=America/Sao_Paulo&useSSL=false&allowPublicKeyRetrieval=true
-CORS_ALLOWED_ORIGINS=http://localhost:${frontPort:-3000}
-
-#Frontend
-FRONTEND_PORT=${frontPort:-3000}
 	" > .env
 }
 
@@ -56,9 +43,5 @@ start_docker () {
   fi
 }
 
-if [ ! -f ".env" ]; then
-	askUser
-fi
-
-echo "Iniciando containers..."
+askUser
 start_docker
