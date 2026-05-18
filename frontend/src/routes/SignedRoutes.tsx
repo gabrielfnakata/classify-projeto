55fdeea43da8bfdd2d4445b3ab96a611af89a4ae
luisa <mluisaaquino3@gmail.com>
2026-05-17 22:21:14 -0300

    fix: correção da rota.

--- frontend/src/routes/SignedRoutes.tsx
+++ frontend/src/routes/SignedRoutes.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ProtectedRoute } from "./ProtectedRoute";
 import Components from "@/app/pages/Components";
+import Dashboard from "@/pages/Dashboard";
 
 export default function SignedRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/home" element={
-                    <ProtectedRoute children={<Components />} />
-                }/>
-            </Routes>
-        </BrowserRouter>
-    )
-};
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={<ProtectedRoute children={<Components />} />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute children={<Dashboard />} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
