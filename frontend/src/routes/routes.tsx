3fb16eae3aae4e482a5758d842b7842945bbf58d
niculinhaz <nicholas.ferraz@itbeta2.com.br>
2026-05-19 00:42:02 -0300

    feat: ajuste no comportamento das rotas

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,9 +1,20 @@
-import { useAuth } from "@/hooks/useAuth"
-import PublicRoutes from "./PublicRoutes";
-import SignedRoutes from "./SignedRoutes";
+import Login from "@/pages/login/Login";
+import OnDevelopment from "@/pages/on-development/OnDevelopment";
+import { Route, Routes } from "react-router";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Components from "@/app/pages/Components";
 
 export default function AppRoutes() {
-    const { signed } = useAuth();
+    return (
+        <Routes>
+            {/* Rotas públicas */}
+            <Route path="/" element={<Login/>}  />
+            <Route path="/forgot-password" element={<OnDevelopment/>} />
 
-    return signed ? <SignedRoutes/>  : <PublicRoutes/>
+            {/* Rotas protegidas */}
+            <Route path="/home" element={
+                <ProtectedRoute children={<Components />} />
+            }/>
+        </Routes>
+    )
 }
