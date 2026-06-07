c796ba6fb65797f26238f3243aedde7b67700179
gabrielfnakata <gabrielnakata@live.com>
2026-06-07 00:56:00 -0300

    [CLA-107] - feat/refactor: adicionada função que realiza a validação de CPFs válidos. Refatorada validação de nulos e strings vazias em outras funções da classe Utils

--- backend/src/main/java/br/com/ifsp/classify/utils/Utils.java
+++ backend/src/main/java/br/com/ifsp/classify/utils/Utils.java
@@ -11,4 +11,41 @@ public class Utils {
     public static <T> boolean hasElements(List<T> list) {
         return !(list == null || list.isEmpty());
     }
+
+    public static String trimAndUpper(String word) {
+        return isNullOrEmpty(word) ? null : word.trim().toUpperCase();
+    }
+
+    public static String removeAllNonDigits(String word) {
+        return isNullOrEmpty(word)? null : word.replaceAll("\\D", "");
+    }
+
+    public static boolean cpfValidator(String cpf) {
+        String formattedCPF = removeAllNonDigits(cpf);
+        if (formattedCPF.length() != 11)
+            return false;
+
+        if (!digitValidation(formattedCPF, true))
+            return false;
+
+        return digitValidation(formattedCPF, false);
+    }
+
+    private static boolean digitValidation(String cpf, boolean isFirstDigit) {
+        int sum = 0;
+        int numberToMultiply = (isFirstDigit)
+            ? 10
+            : 11;
+        int length = (isFirstDigit)
+            ? 9
+            : 10;
+
+        for (int c = 0; c < length; ++c) {
+            int digit = Character.getNumericValue(cpf.charAt(c));
+            sum += (digit * numberToMultiply);
+            --numberToMultiply;
+        }
+
+        return Character.getNumericValue(cpf.charAt(length)) == ((sum * 10) % 11);
+    }
 }
