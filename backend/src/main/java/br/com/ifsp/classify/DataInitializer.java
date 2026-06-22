83aca1205576048a2a969c7a723f44631281b856
Larissa Lumy <larissa.lumy77@gmail.com>
2026-06-22 01:26:10 -0300

    [CLA-60] - chore: adicionando dados na tabela de disciplina

--- backend/src/main/java/br/com/ifsp/classify/DataInitializer.java
+++ backend/src/main/java/br/com/ifsp/classify/DataInitializer.java
@@ -8,20 +8,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.ifsp.classify.dtos.create.EmployeeCreateDTO;
+import br.com.ifsp.classify.dtos.create.SubjectCreateDTO;
+import br.com.ifsp.classify.dtos.create.SubjectTeacherCreateDTO;
 import br.com.ifsp.classify.dtos.create.RoleCreateDTO;
 import br.com.ifsp.classify.dtos.create.UserCreateDTO;
+import br.com.ifsp.classify.dtos.get.EmployeeGetDTO;
 import br.com.ifsp.classify.dtos.get.RoleGetDTO;
+import br.com.ifsp.classify.dtos.get.SubjectGetDTO;
 import br.com.ifsp.classify.repositories.RoleRepository;
 import br.com.ifsp.classify.repositories.UserRepository;
 import br.com.ifsp.classify.services.EmployeeService;
 import br.com.ifsp.classify.services.RoleService;
+import br.com.ifsp.classify.services.SubjectService;
+import br.com.ifsp.classify.services.SubjectTeacherService;
 import br.com.ifsp.classify.services.UserService;
 
 @Configuration
 public class DataInitializer {
     
     @Bean
-    CommandLineRunner initDatabase(RoleRepository roleRepository, RoleService roleService, EmployeeService employeeService, UserRepository userRepository, UserService userService) {
+    CommandLineRunner initDatabase(RoleRepository roleRepository, RoleService roleService, EmployeeService employeeService, UserRepository userRepository, UserService userService, SubjectService subjectService, SubjectTeacherService subjectTeacherService) {
         return (args) -> {
             if (roleRepository.count() == 0) {
                 roleService.create(new RoleCreateDTO("ADMIN", "Cargo máximo, possui todas as permissões"));
@@ -31,7 +37,8 @@ public class DataInitializer {
 
             if (userRepository.count() == 0) {
                 RoleGetDTO adminRole = roleService.getById("ADMIN");
-                if (adminRole == null)
+                RoleGetDTO profRole = roleService.getById("PROFE");
+                if (adminRole == null || profRole == null)
                     return;
 
                 employeeService.create(new EmployeeCreateDTO(
@@ -47,6 +54,22 @@ public class DataInitializer {
                     ),
                     null
                 ));
+                
+                EmployeeGetDTO teacherEmployee = employeeService.create(new EmployeeCreateDTO(
+                    "GLAUCO",
+                    LocalDate.of(2000, Month.JANUARY, 01),
+                    "417.116.320-07",
+                    LocalDate.now(),
+                    new UserCreateDTO(
+                        "glauco@gmail.com",
+                        "123456aA",
+                        profRole.id(),
+                        null
+                    ),
+                    null
+                ));
+                SubjectGetDTO mathSubject = subjectService.create(new SubjectCreateDTO("Matemática"));
+                subjectTeacherService.create(new SubjectTeacherCreateDTO(teacherEmployee.uuid(), mathSubject.uuid()));
             }
         };
     }
