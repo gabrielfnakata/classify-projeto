fa609305b9a48ca3b7eb2a750bc5bce32d3ba39a
glaucolucca <glaucodelucca@gmail.com>
2026-08-17 16:45:13 -0300

    [CLA-110] - feat: adiciona helper de cores para os gráficos

---
+++ frontend/src/lib/chart-colors.ts
@@ -0,0 +1,12 @@
+export const VIZ_SERIES = [
+  "var(--viz-series-1)",
+  "var(--viz-series-2)",
+  "var(--viz-series-3)",
+  "var(--viz-series-4)",
+  "var(--viz-series-5)",
+  "var(--viz-series-6)",
+  "var(--viz-series-7)",
+  "var(--viz-series-8)",
+] as const
+
+export const VIZ_PRIMARY = VIZ_SERIES[0]
